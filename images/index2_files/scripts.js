15e3807be799b4f7ab81b8b8810782952fa6548b
Vikram <vikram@growthplug.com>
2021-03-22 21:16:41 +0530

    remove form from gallery

---
+++ images/index2_files/scripts.js
@@ -0,0 +1,29 @@
+jQuery(document).ready(function($) {
+	/*-------------------------------------
+	Donation Others Toggle
+	-------------------------------------*/
+	$('#donationForm').on('change', function() {
+		if($('#donation-custom-toggle').is(':checked')){
+			$('#custom-donation-ammount').addClass('input-show');
+			console.log('clecked');
+		} else {
+			$('#custom-donation-ammount').removeClass('input-show');
+		}
+	});
+	
+	$(".number").keydown(function (e) {
+		// Allow: backspace, delete, tab, escape, enter and .
+		if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
+		  // Allow: Ctrl+A, Command+A
+		 (e.keyCode === 65 && (e.ctrlKey === true || e.metaKey === true)) || 
+		  // Allow: home, end, left, right, down, up
+		 (e.keyCode >= 35 && e.keyCode <= 40)) {
+		   // let it happen, don't do anything
+		   return;
+		}
+		// Ensure that it is a number and stop the keypress
+		if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+		 e.preventDefault();
+		}
+	});
+});
